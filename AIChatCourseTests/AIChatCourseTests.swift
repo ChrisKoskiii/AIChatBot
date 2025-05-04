99ba14b18d08713b5d3865a6504818c7a00e6789
Christopher Koski <ckoski18@gmail.com>
2025-05-04 16:01:58 -0400

    Fix nested repo issue and re-add project files

---
+++ AIChatCourseTests/AIChatCourseTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseTests.swift
+//  AIChatCourseTests
+//
+//  Created by Christopher Koski on 5/4/25.
+//
+
+import Testing
+@testable import AIChatCourse
+
+struct AIChatCourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
