4c2346949345e51cd6dc13390f567ffae2e02ec5
Christopher Koski <ckoski18@gmail.com>
2025-05-06 20:50:37 -0400

    [Feature] Add WelcomeView UI

---
+++ AIChatCourse/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  AIChatCourse
+//
+//  Created by Christopher Koski on 5/6/25.
+//
+
+import Foundation
+
+struct Constants {
+  static let randomImage = "https://picsum.photos/600/600"
+  static let privacyPolicyUrl = "https://www.google.com"
+  static let termsOfServiceUrl = "https://www.google.com"
+}
