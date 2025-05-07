9de0fe2512e414e45092dbadd3d94785aefd1598
Christopher Koski <ckoski18@gmail.com>
2025-05-07 15:37:47 -0400

    [Feature] OnboardingIntroView & OnboardingColorView

---
+++ AIChatCourse/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingColorView.swift
+//  AIChatCourse
+//
+//  Created by Christopher Koski on 5/7/25.
+//
+
+import SwiftUI
+
+struct OnboardingColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingColorView()
+}
---
+++ AIChatCourse/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingIntroView.swift
+//  AIChatCourse
+//
+//  Created by Christopher Koski on 5/7/25.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingIntroView()
+}
