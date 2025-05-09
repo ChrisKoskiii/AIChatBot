4e01d5907fa772c7669b9bf5cc7b68290ef62eb9
Christopher Koski <ckoski18@gmail.com>
2025-05-08 20:51:44 -0400

    [Feature] OnboardingCompleted View

--- AIChatCourse/Core/Onboarding/ColorView/OnboardingColorView.swift
+++ AIChatCourse/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -8,11 +8,69 @@
 import SwiftUI
 
 struct OnboardingColorView: View {
+  
+  @State private var selectedColor: Color?
+  let profileColors: [Color] = [.red, .green, .orange, .blue, .yellow, .purple, .pink, .indigo, .mint]
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      ScrollView {
+        colorGrid
+          .padding(.horizontal, 24)
+      }
+      .safeAreaInset(edge: .bottom, alignment: .center, spacing: 16, content: {
+        ZStack {
+          if let selectedColor {
+            ctaButton(selectedColor: selectedColor)
+            .transition(AnyTransition.move(edge: .bottom))
+          }
+        }
+        .padding(24)
+        .background(Color(uiColor: .systemBackground))
+      })
+      .animation(.bouncy, value: selectedColor)
+      .toolbar(.hidden, for: .navigationBar)
     }
+  
+  private func ctaButton(selectedColor: Color) -> some View {
+    NavigationLink {
+      OnboardingCompletedView(selectedColor: selectedColor)
+    } label: {
+      Text("Continue")
+        .callToActionButton()
+    }
+  }
+  
+  private var colorGrid: some View {
+    LazyVGrid(
+      columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 3),
+      alignment: .center,
+      spacing: 16,
+      pinnedViews: [.sectionHeaders]) {
+        Section(content: {
+          ForEach(profileColors, id: \.self) { color in
+            Circle()
+              .fill(.accent)
+              .overlay(content: {
+                color
+                  .clipShape(Circle())
+                  .padding(selectedColor == color ? 10 : 0)
+              })
+              .foregroundStyle(color)
+              .onTapGesture {
+                selectedColor = color
+              }
+          }
+        }, header: {
+          Text("Select a profile color")
+            .font(.headline)
+        })
+      }
+  }
 }
 
 #Preview {
+  NavigationStack {
     OnboardingColorView()
+  }
+  .environment(AppState())
 }
